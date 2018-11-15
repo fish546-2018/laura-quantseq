b79e0b9dcd262c47aa718852d32d0491b31cf979
laurahspencer <lhs3@uw.edu>
2018-11-14 17:32:50 -0800

    scrip to download illumina data

---
+++ scripts/download-quantseq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+    set -e
+    set -u
+    set -o pipefail
+
+# create directory to house sequence files  
+mkdir ../data/illumina-raw/
+
+# Download raw quantseq data files (already demultiplexed) 
+
+wget --accept "*.fastq.gz" \
+--no-directories --recursive --no-parent \
+http://owl.fish.washington.edu/wetgenes/20181109_QS_Laura/ \
+-P ../data/illumina-raw/
+
+# create a .txt file for downloaded quantseq file names 
+touch ../data/illumina-raw/quatseq-files.txt 
+
+# print the file names to a text file 
+ls ../data/illumina-raw/*.fastq.gz >> ../data/illumina-raw/quatseq-files.txt 
+
+# return list of files downloaded 
+echo "downloaded the following files:"
+cat ../data/illumina-raw/quatseq-files.txt
