57f3ad451d3ff90423f091c559bebd61a7080e82
laura spencer <laura@D-10-19-95-240.dhcp4.washington.edu>
2018-11-16 15:35:31 -0800

    hopefully this works

--- scripts/download-quantseq.sh
+++ scripts/download-quantseq.sh
@@ -13,11 +13,17 @@ wget --accept "*.fastq.gz" \
 http://owl.fish.washington.edu/wetgenes/20181109_QS_Laura/ \
 -P ../data/illumina-raw/
 
+# unzip 
+gunzip -k ../data/illumina-raw/*.fastq.gz
+
+# remove zipped files 
+rm ../data/illumina-raw/*.gz
+
 # create a .txt file for downloaded quantseq file names 
 touch ../data/illumina-raw/quatseq-files.txt 
 
 # print the file names to a text file 
-ls ../data/illumina-raw/*.fastq.gz >> ../data/illumina-raw/quatseq-files.txt 
+ls ../data/illumina-raw/*.fastq >> ../data/illumina-raw/quatseq-files.txt 
 
 # return list of files downloaded 
 echo "downloaded the following files:"
